1db583a96f898341e532ce8f97971e04478d1fd6
Matt Switlik <switlikm@gmail.com>
2018-12-04 21:48:59 -0500

    flask template displaying schedule.json

--- allyearlights-web.py
+++ allyearlights-web.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import render_template
+import os
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def main():
+    dirpath = os.path.dirname(os.path.realpath(__file__))
+    with open(dirpath + '/schedule.json') as f:
+        schedule = json.load(f)
+        f.close()
+
+    return render_template('index.html', title='AllYearLights', schedule=schedule)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
